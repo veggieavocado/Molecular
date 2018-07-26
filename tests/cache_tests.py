b9563e040353661ce6994a78540cf0cd94f75110
1 <ppark9553@gmail.com>
2018-07-26 16:16:00 +0900

    테스트 통과시키기

--- tests/cache_tests.py
+++ tests/cache_tests.py
@@ -16,16 +16,14 @@ from django.core.cache import cache
 ## Test 시작 ##
 @pytest.fixture
 def cache_data():
-    return '장고 캐시 데이터'
-
-def test_cache_can_set_key(cache_data):
+    cache_data = '장고 캐시 데이터'
     res = cache.set('test_cache_key', cache_data)
-    assert res == True
+    assert res == True # 캐시 데이터를 저장할 수 있다
+    return cache_data
 
-def test_cache_can_get_key(cache_data):
+def test_cache_can_get_and_delete_key(cache_data):
     value = cache.get('test_cache_key')
     assert value == cache_data
 
-def test_cache_can_delete_key():
     res = cache.delete('test_cache_key')
     assert res == True

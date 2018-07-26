1e84c5177b4fad51e19e847272e924d809f5e89d
1 <ppark9553@gmail.com>
2018-07-26 15:30:53 +0900

    도커 컨테이너 2개 트레비스에 추가

--- molecular/settings.py
+++ molecular/settings.py
@@ -1,22 +1,15 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+TESTING = os.environ.get('TRAVIS', 'False')
+DOCKER = os.environ.get('DOCKER_CONTAINER', 'False')
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fv($0e1to(r8ddlr4e802nl7i#42sm0k8b(mc-4oor8el%^z-p'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -57,20 +50,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'molecular.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -87,10 +72,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -101,17 +82,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
 
-if os.environ.get('TRAVIS', 'False') == 'True':
+if TESTING == 'True' or DOCKER == 'False':
     cache_location = 'redis://localhost:6379/'
 else:
     cache_location = 'redis://redis:6379/'
-    
+
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
